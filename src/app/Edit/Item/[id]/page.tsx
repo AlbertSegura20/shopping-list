a72924d8d9830adde31f3c751c303a92980aa4b9
Albert Segura Martinez <seguramartinezal@gmail.com>
2023-08-01 00:29:47 -0400

    refactor category e item

--- src/app/Edit/Item/[id]/page.tsx
+++ src/app/Edit/Item/[id]/page.tsx
@@ -1,35 +1,39 @@
 'use client'
 import React, {useEffect, useState} from "react";
-import {CustomContext} from "@/Context/Context";
 import {useRouter} from "next/navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-// import "../../../../components/Aside_Menubar.css";
-import Link from "next/link";
 import Aside_Menubar from "@/components/Aside_Menubar";
 import { Table, Button, Dropdown } from '@nextui-org/react';
-import Image from "next/image";
 import "./Id.css";
 
 
 const Item = () => {
 
-    const [text, setText] = useState<any>([])
     const credentialsUser = "lsegura:12345";
     const credentialsBase64User = btoa(credentialsUser);
-    const [visible, setVisible] = React.useState(false);
+    // const [visible, setVisible] = React.useState(false);
     const Router = useRouter();
     const userName = sessionStorage.getItem('userName');
     const [token, setToken] = useState<any>();
     const [session, setSession] = useState<any>();
+    const [categories, setCategories] = useState<any>([]);
+    const [items, setItems] = useState<any>([]);
     const [objectItem, setObjectItem] = useState<any>({});
+    // const [itemName, setItemName] = useState<string>();
+    // const [itemPrice, setItemPrice] = useState<any>();
+    const [itemCategory, setItemCategory] = useState<number>();
+    const [idCategory, setIdCategory] = useState<number>();
+    const [selectedCategory, setSelectedCategory] = useState<string>();
+    // const [id, setId] = useState();
 
     useEffect(() => {
         if(!userName){
             Router.push("/");
         }
-        getList();
+        getItem();
         getToken();
+        getCategory();
 
     }, []);
 
@@ -60,10 +64,9 @@ const Item = () => {
 
 
     }
+    async function getCategory() {
 
-    async function getList() {
-
-        const result =  await fetch("http://www.code2ever.com:8080/api/list", {
+       const result = await fetch("http://www.code2ever.com:8080/api/category", {
             method: 'GET',
             mode: 'cors',
             cache: 'default',
@@ -76,16 +79,144 @@ const Item = () => {
             referrerPolicy: 'no-referrer',
         }).then(response => {
             return response.json();
-        }).then(data => data);
+        });
+        setCategories(result.data);
+    }
+    async function getItem() {
 
-        setText(result.data);
+       const result = await fetch("http://www.code2ever.com:8080/api/item", {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Basic " + credentialsBase64User,
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+        }).then(response => {
+            return response.json();
+        });
+
+       setItems(result.data);
+
+    }
+
+    const handleInput = ({target}:{target:any}) => {
+
+        if(target.name === "Name"){
+            objectItem.name = target.value;
+        }
+        if(target.name === "Price"){
+            objectItem.price = target.value;
+        }
+    }
+
+    const handleChange = (data:any) => {
+        setItemCategory(data.target.value);
+        console.log(itemCategory);
+
+    }
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+
+        if(objectItem.id){
+            updateItem(e);
+            objectItem.id = null;
+            return;
+        }
+        saveItem(e);
 
 
+    }
+    async function saveItem(e:any) {
+        e.preventDefault();
+
+        const itemObject = {
+            "name":  objectItem.name,
+            "price":  objectItem.price,
+            "idCategory": Number(itemCategory)
+        }
+
+        await fetch("http://www.code2ever.com:8080/api/item", {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'default',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                "Session": session,
+                "X-CSRF-TOKEN": token,
+                "Authorization": "Basic " + credentialsBase64User,
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(itemObject)
+        }).then(response => console.log(response.status));
+
+        getItem();
+        e.target.reset();
+    }
+
+    async function deleteItem(id:any) {
+
+
+        const data = new URLSearchParams();
+        data.append('id', id);
+
+        await fetch("http://www.code2ever.com:8080/api/item", {
+            method: 'DELETE',
+            mode: 'cors',
+            cache: 'default',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                "Session": session,
+                "X-CSRF-TOKEN": token,
+                "Authorization": "Basic " + credentialsBase64User,
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: data
+        }).then(response => console.log(response.status));
+        getItem();
+    }
+
+    async function updateItem(e:any) {
+        e.preventDefault();
+        const item = {
+            "id":objectItem.id,
+            "name": objectItem.name,
+            "price": objectItem.price,
+            "idCategory":itemCategory
+        }
+
+
+        await fetch("http://www.code2ever.com:8080/api/item", {
+            method: 'PUT',
+            mode: 'cors',
+            cache: 'default',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                "Session": session,
+                "X-CSRF-TOKEN": token,
+                "Authorization": "Basic " + credentialsBase64User,
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(item)
+        }).then(response => console.log(response.status));
+        getItem();
+        // setObjectItem("");
     }
 
     const ItemSelected = (data:any) => {
         console.log(data);
         setObjectItem(data);
+        setIdCategory(data.idCategory);
     }
 
 
@@ -130,38 +261,31 @@ const Item = () => {
                       <div className={"form-container "}>
                           <div className={"d-flex align-items-center justify-content-center mt-3"}>
 
-                              <form className={"shadow-lg rounded px-3 py-3 border form-container-edit   "} id={"formContainer"} style={{width: "400px"}}>
+                              <form onSubmit={handleSubmit} className={"shadow-lg rounded px-3 py-3 border form-container-edit   "} id={"formContainer"} style={{width: "300px"}}>
                                   <div className="mb-3">
                                       <label htmlFor="exampleInputItem" className="form-label">Name</label>
-                                      <input id={"input-name"} name={"Name"} defaultValue={objectItem?.name} type="text" className="form-control" placeholder={"Item"}/>
+                                      <input id={"input-name"} name={"Name"} defaultValue={objectItem?.name} onInput={handleInput} type="text" className="form-control" placeholder={"Item"}/>
                                   </div>
                                   <div className="mb-3">
                                       <label htmlFor="exampleInputPrice" className="form-label">Price</label>
-                                      <input id={"input-price"} name={"Price"} defaultValue={objectItem?.total} type="number" className="form-control"  placeholder={'0.00'}/>
+                                      <input id={"input-price"} name={"Price"} defaultValue={objectItem?.price} onInput={handleInput} type="number" className="form-control"  placeholder={'0.00'}/>
                                   </div>
 
 
-                                <div className={"d-flex"}> Choose a category
+                                <div>Category
+                                    <div className={"mt-1 pb-auto"}>
 
-                                    <div className={"ms-2 pb-auto"}>
-                                        <Dropdown>
-                                            <Dropdown.Button flat color={"secondary"} size={"xs"}>Category</Dropdown.Button>
-                                            <Dropdown.Menu aria-label="Static Actions" >
-                                                <Dropdown.Item key="new">New file</Dropdown.Item>
-                                                <Dropdown.Item key="copy">Copy link</Dropdown.Item>
-                                                <Dropdown.Item key="edit">Edit file</Dropdown.Item>
-                                                <Dropdown.Item key="delete" color="error">
-                                                    Delete file
-                                                </Dropdown.Item>
-                                            </Dropdown.Menu>
-                                        </Dropdown>
+                                        <select value={idCategory} className={"px-2 rounded"} aria-label="Default select example" onChange={handleChange} required>
+                                            <option value="" className={"align-items-center"}>Category</option>
+                                            {categories?.map((category:any, index:number) => (
+                                                <option value={category.id} key={index}>{category.description}</option>))}
+                                        </select>
                                     </div>
 
                                 </div>
 
                                   <div className={"d-flex justify-content-center mt-3"}>
                                       <button id={"button-submit-edit"} type="submit" className="btn btn-primary me-2">Submit</button>
-                                      <button id={"button-submit-edit"} type="submit" className="btn text-white" style={{backgroundColor:"#0D98BA"}}>Update</button>
                                   </div>
                               </form>
 
@@ -173,8 +297,6 @@ const Item = () => {
 
 
                     <div className={"table-container1 d-flex justify-content-center me-5 pe-5"}>
-
-
 
                         <div className={"table-container border  rounded mt-4"}>
 
@@ -191,24 +313,26 @@ const Item = () => {
                             >
 
                                 <Table.Header>
-                                    <Table.Column>Id</Table.Column>
                                     <Table.Column>Name</Table.Column>
                                     <Table.Column>Price</Table.Column>
                                     <Table.Column>Category</Table.Column>
-                                    <Table.Column></Table.Column>
+                                    <Table.Column>Edit</Table.Column>
+                                    <Table.Column>Delete</Table.Column>
                                 </Table.Header>
 
                                 <Table.Body>
-                                    {text.map((items:any) => (
-                                       <Table.Row key={items.id}>
-                                           <Table.Cell><div onClick={() => ItemSelected(items)}>{items.id}</div></Table.Cell>
-                                           <Table.Cell><div onClick={() => ItemSelected(items)}>{items.name}</div></Table.Cell>
-                                           <Table.Cell><div onClick={() => ItemSelected(items)}>{items.total}</div></Table.Cell>
-                                           <Table.Cell><div onClick={() => ItemSelected(items)}>{items.totalItems}</div></Table.Cell>
-                                           <Table.Cell>  <Button color="error" auto>
-                                               X
-                                           </Button></Table.Cell>
-                                       </Table.Row>
+                                    {items.map((item:any) => (
+                                        <Table.Row key={item.id}>
+                                            <Table.Cell><div>{item.name}</div></Table.Cell>
+                                            <Table.Cell><div>{item.price}</div></Table.Cell>
+                                            <Table.Cell><div>{categories.filter((category:any) => category.id === item.idCategory)[0]?.description}</div></Table.Cell>
+                                            <Table.Cell>  <Button color="success" auto onPress={() => ItemSelected(item)}>
+                                                Edit
+                                            </Button></Table.Cell>
+                                            <Table.Cell>  <Button color="error" auto onPress={() => deleteItem(item.id)}>
+                                                X
+                                            </Button></Table.Cell>
+                                        </Table.Row>
 
                                     ))}
                                 </Table.Body>
